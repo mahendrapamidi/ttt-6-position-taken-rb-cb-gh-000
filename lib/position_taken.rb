1a6ec673807ad3cada071530cf17847acaa769c3
Mahendra Pamidi <mahendrapamidi96@gmail.com>
2017-12-08 17:01:18 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ def position_taken?(board,index)
     return false
   elsif(board[index]=" ")
     return false
-  elsif(board[index]='X'||board[index]='O')
+  elsif(board[index]='X'|board[index]='O')
     return true
       end
 end
